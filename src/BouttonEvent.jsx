11ebc16b0fb587225e7042783f1f787d44c03ff8
Albi <allubila016@gmail.com>
2023-12-16 16:01:24 +0100

    adding Event logic to add Task on the list

---
+++ src/BouttonEvent.jsx
@@ -0,0 +1,11 @@
+export default function AddTask({ isVisible }) {
+  if (!isVisible) {
+    return null; // Render nothing if isVisible is false
+  }
+
+  return (
+    <div>
+      <h1>d</h1>
+    </div>
+  );
+}
